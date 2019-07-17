1a656c77c144fb230f84d61aa2efffd49cbd9150
Anthony Michael Stephens <mike@anthonys-mbp.lan>
2019-07-17 15:00:05 -0400

    challenge 1 complete

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
-console.log('Challenge #1');
-
 // Console out the numbers from 1-20, but only if they're divisible by 3.
-
 // Use `if` and a modulus operation.
+let done = false;
+const nums = 20;
+let i = nums;
+for (i = 1; i < nums; i++) {
+  if (i % 3 === 0) {
+    console.log("Challenge #1" + " " + i);
+  }
+}
+// console.log("Challenge #2");
 
-
-console.log('Challenge #2');
-
+// while (!done && i < nums) {
+//   const divisBy3 = i % 3 === 0;
+//   console.log("Challenge #2" + " " + i);
+// }
 // Same problem as #1, but no `if`s, `mods`, or `but`s (and no, that last one is
 // not a thing). If you did it right, you should run the loop 6 times with 1
 // console log per loop. That's called... a clue!
 
-
-console.log('Challenge #3');
+console.log("Challenge #3");
 
 // Console out the numbers from 1-20. BACKWARDS.
 
@@ -22,42 +28,35 @@ console.log('Challenge #3');
 
 // (No extra credit will be given for wearing heels during this challenge.)
 
-
-console.log('Challenge #4');
+// console.log('Challenge #4');
 
 // For the string 'Boy howdy am I good at this!', console out the letters
 // individually.
 
-
-console.log('Challenge #5');
+// console.log('Challenge #5');
 
 // For the string, 'And getting better every day.', starting with the second
 // character, print out every third character.
 
-
-console.log('Challenge #6');
+// console.log('Challenge #6');
 
 // For the string 'Am I the best?', console out the letters individually, AND
 // UPPERCASED.
 
-
-console.log('Challenge #7');
+// console.log('Challenge #7');
 
 // For the string 'Whoa, I am the best!', console out the letters individually,
 // but also capitalize the letters in the third word.
 
-
-console.log('Challenge #8');
-
+// console.log('Challenge #8');
 
 // For the string 'I am become death, destroyer of worlds.', console out the letters
 //  individually, but backwards. That is, the first console log should be
 // `.`, followed by `s`, followed by `d`, and so on.
 
+// console.log('Challenge #9');
 
-console.log('Challenge #9');
-
-const names = 'Colin|Mesuara|Drew|In Seop|Emma|Ben|J|Ryan|Olgy|Mike|Chris|John';
+const names = "Colin|Mesuara|Drew|In Seop|Emma|Ben|J|Ryan|Olgy|Mike|Chris|John";
 
 // Console out the individual letters of each person in class today, which are
 // in the string above. Every time you start a new name (including the first
@@ -66,4 +65,3 @@ const names = 'Colin|Mesuara|Drew|In Seop|Emma|Ben|J|Ryan|Olgy|Mike|Chris|John';
 // You'll know you're at a new person's name when you hit the pipe character.
 
 // Don't print the pipes!
-
