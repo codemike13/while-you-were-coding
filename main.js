1713d28f2a4621e9139f40513d2ea11d2f88c0c7
Anthony Michael Stephens <mike@anthonys-mbp.lan>
2019-07-17 16:01:11 -0400

    update solutions

--- main.js
+++ main.js
@@ -3,6 +3,9 @@
 let done = false;
 const nums = 20;
 let i = nums;
+const howdyString = "Boy howdy am I good at this!";
+const gettingBetter = "And getting better every day.";
+
 for (i = 1; i < nums; i++) {
   if (i % 3 === 0) {
     console.log("Challenge #1" + " " + i);
@@ -18,37 +21,62 @@ for (i = 1; i < nums; i++) {
 // not a thing). If you did it right, you should run the loop 6 times with 1
 // console log per loop. That's called... a clue!
 
-console.log("Challenge #3");
+console.log("Challenge #3//////////////////////////");
 
 // Console out the numbers from 1-20. BACKWARDS.
+for (i = 20; i >= 1; i--) {
+  if (i % 3 === 0) {
+    console.log("Challenge #3" + " " + i);
+  }
+}
 
-// (And, optionally, in heels.)
+console.log("Challenge #4/////////////////////////");
 
-// (That's a reference that only Olds make, and only if they're SUPER CULTURED.)
-
-// (No extra credit will be given for wearing heels during this challenge.)
-
-// console.log('Challenge #4');
+for (i = 0; i < howdyString.length; i++) {
+  console.log(howdyString[i]);
+}
 
 // For the string 'Boy howdy am I good at this!', console out the letters
 // individually.
 
-// console.log('Challenge #5');
+console.log("Challenge #5/////////////////////////");
+for (i = 0; i < howdyString.length; i++) {
+  console.log(howdyString[i]);
+}
 
 // For the string, 'And getting better every day.', starting with the second
 // character, print out every third character.
 
-// console.log('Challenge #6');
+console.log("Challenge #6///////////////////////");
+const theBestyet = 'Am I the best!';
 
+for (i = 1; i < gettingBetter.length; i++) {
+  console.log(gettingBetter[i]);
+}
 // For the string 'Am I the best?', console out the letters individually, AND
 // UPPERCASED.
 
-// console.log('Challenge #7');
+const theBest = 'Whoa, I am the best!';
+let spaceCount = 0;
+for (i = 0; i < theBest.length; i++) {
+    if (theBest[i] === " ") {
+        spaceCount += 1;
+    }
+    if (spaceCount === 2) {
+        console.log(theBest[i].toUpperCase());
+    } else {
+        console.log(theBest[i]);
+    }
+}
 
 // For the string 'Whoa, I am the best!', console out the letters individually,
 // but also capitalize the letters in the third word.
 
-// console.log('Challenge #8');
+console.log('Challenge #8//////////////////////////////');
+const death = 'I am become death, destroyer of worlds.';
+for (i = death.length; i > 0; --i) {
+console.log(death[i]);
+}
 
 // For the string 'I am become death, destroyer of worlds.', console out the letters
 //  individually, but backwards. That is, the first console log should be
